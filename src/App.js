ebe7be4494f1f81b70d2e0913a88df27fe6f192b
John Reed <contact.johnreed@gmail.com>
2021-04-01 16:51:34 -0400

    need some help on the searchForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <Container>
+            <h1>Search App</h1>
+            <p>This is a simple search app</p>
+            <SearchForm />
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default App;
